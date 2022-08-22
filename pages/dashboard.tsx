d6b0e8d446a94a0ac428163fbb8d6122dafdf8e0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-08-23 08:42:26 +0900

    ダッシュボードにタスク一覧表示

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from 'next'
-import { LogoutIcon } from '@heroicons/react/solid'
+import {
+  LogoutIcon,
+  StatusOfflineIcon,
+  DocumentTextIcon,
+} from '@heroicons/react/solid'
 import { supabase } from '../utils/supabase'
 import { Layout } from '../components/Layout'
+import { TaskList } from '../components/TaskList'
+import { TaskForm } from '../components/TaskForm'
 
 const Dashboard: NextPage = () => {
   const signOut = () => {
@@ -13,6 +19,15 @@ const Dashboard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+      <div className="grid grid-cols-2 gap-40">
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className="2-8 h-8 text-blue-500" />
+          </div>
+          <TaskForm />
+          <TaskList />
+        </div>
+      </div>
     </Layout>
   )
 }
